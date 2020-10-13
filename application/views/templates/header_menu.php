b705ccdd0d72c70e8aeaf97785225a9af92734c8
haxan100 <abdul.hasan388@gmail.com>
2020-10-14 00:46:45 +0700

    menampilkan gambar

--- application/views/templates/header_menu.php
+++ application/views/templates/header_menu.php
@@ -28,6 +28,17 @@
 						</li>
 					</ul>
 				</div>
+				<?php 
+				// var_dump($_SESSION);
+				$id_admin = $_SESSION['id_admin'];
+				$sql ="SELECT * FROM admin where id_admin = $id_admin";
+				$query = $this->db->query($sql);
+				$user = $query->result();
+				$user = $user[0];
+
+				// var_dump($user->image);die;
+
+				?>
 
 
 
@@ -38,7 +49,8 @@
 								<div class="widget-content-left">
 									<div class="btn-group">
 										<a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
-											<img width="42" class="rounded-circle" src="<?php echo base_url() . "templates/architectui-html-free/"; ?>assets/images/avatars/1.jpg" alt="">
+											<img width="42" class="rounded-circle" data-toggle="modal" data-target="#exampleModal" src="<?php echo base_url() . "upload/images/admin/".$user->image; ?>" alt="">
+							
 											<i class="fa fa-angle-down ml-2 opacity-8"></i>
 										</a>
 										<div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
@@ -68,4 +80,24 @@
 						</div>
 					</div>
 				</div>
+			</div>
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					...
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+				</div>
+			</div>
 			</div>

e70443849549949d075a17888ec39be551503855
haxan100 <abdul.hasan388@gmail.com>
2020-11-08 22:55:26 +0700

    bugs fixing list dan document

---
+++ application/core/MY_nextListOrBugsFixing.php
@@ -0,0 +1,18 @@
+<?php
+// user:
+// - saat menambah keranjangm , cek user, apakah masuk/ login atau belum,
+// jika belum di lempar untuk suruh login / register
+
+// // register:- cek jika sudah atau ada yang lain udah menggunakan(email , no telpon)
+// - hapus floating cart
+// - tampilan mobile , di dalam cekout, nama dan qty dan foto mepet
+// ketika cekout, milih kurir , di kasih loading
+// di dialam halaman pembayaran , masih eror , ketika user sudah ada transaksi , dia tetap menampilkan yang lama
+// halaman pesanan masih loading yang aneh
+// halaman pesanan saya , klik detail tampilkan detailnya 
+
+
+// admin :
+// - di dalam produk , ada tombo aktiv / tidak aktiv
+// dinamiskan tetang kami, informasi lainya
+// dinamiskan hubungi kami, informasi lainya

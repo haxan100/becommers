87cdbdcd6c0e451d407af5ddc08550154fc1e98d
haxans100 <agostand@gmail.com>
2021-05-25 16:53:23 +0700

    login register

---
+++ application/controllers/Login.php
@@ -0,0 +1,22 @@
+<?php 
+        
+defined('BASEPATH') OR exit('No direct script access allowed');
+        
+class Login extends CI_Controller {
+
+public function index()
+{
+    $this->load->view('Login/Login');
+    
+                
+}
+public function process_login()
+{
+    # code...
+}
+        
+}
+        
+    /* End of file  Login.php */
+        
+                            

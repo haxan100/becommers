c059c492ebd93e47bb2fb450bda7f3d5ed36ccef
hasan <abdul.hasan388@gmail.com>
2021-06-01 23:23:41 +0700

    typo kode transaksi

--- application/views/User/pesananNanti.php
+++ application/views/User/pesananNanti.php
@@ -175,8 +175,6 @@ if (isset($_SESSION['id_user'])) {
 	</div>
 </div>
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 <script>
 	$(document).ready(function() {
@@ -260,7 +258,7 @@ if (isset($_SESSION['id_user'])) {
 				'				<br><span>' +
 				produk.created_at +
 				'<br><span>' +
-				'					Kode Transaksi:' +
+				'					Kode:' +
 				produk.kode_transaksi +
 				'			<div>' +
 				'Harga : Rp.' +

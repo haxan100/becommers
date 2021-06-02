fe5cefd328b0139f44588001a6a735b89bde5faa
hasan <abdul.hasan388@gmail.com>
2021-06-02 23:22:11 +0700

    routes sementara

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,5 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'user';
+$route['profile'] = 'user/profile';
+$route['Profile'] = 'user/profile';
+
+$route['about'] = 'user/about';
+$route['About'] = 'user/about';
+
+$route['hubungi'] = 'user/hubungi';
+$route['Hubungi'] = 'user/hubungi';
+
+$route['Pesanan'] = 'user/Pesanan';
+$route['Pesanan'] = 'user/Pesanan';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
